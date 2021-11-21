353828ce67e79bcd9aabc45a3ee0d99610366f1d
Nik <sau3000@mail.ru>
2021-11-22 01:05:47 +0300

    add new project

---
+++ nik/src/components/My-projects/Ojjo/Burger.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="header-burger">
+    
+    <Slide right v-if="this.burger">
+      <ul class="burger__items">
+        <v-img
+          src="~@/assets/OJJO/images/logo.svg"
+          class="burger__logo"
+        ></v-img>
+        <li class="burger__link" v-for="i in header__link" :key="i">{{ i }}</li>
+        <div class="burger__border"></div>
+        <div class="burger__icon pt-6 pb-6">
+          <v-btn icon class="mr-8">
+            <v-icon large color="white"> mdi-account </v-icon>
+          </v-btn>
+          <v-btn icon class="mr-2">
+            <v-icon large color="white"> mdi-heart </v-icon>
+          </v-btn>
+        </div>
+        <div class="burger__border"></div>
+        <div class="burger__footer-icon pt-6 pb-6">
+          <v-btn icon class="mr-4" v-for="i in footer_icon" :key="i">
+            <v-icon large color="white"> {{ i }} </v-icon>
+          </v-btn>
+        </div>
+      </ul>
+    </Slide>
+  </div>
+</template>
+
+<script>
+import { Slide } from "vue-burger-menu";
+
+export default {
+  components: {
+    Slide, // Register your component
+  },
+  mounted() {},
+  props: ["burger"],
+  data() {
+    return {
+      header__link: [
+        "Контрагентам",
+        "Дизайнерам",
+        "Вакансии",
+        "Вход/Регистрация",
+      ],
+      footer_icon: ["mdi-youtube", "mdi-facebook"],
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style>
+
+.burger__border {
+  border-bottom: 1px solid #ffffff;
+  width: 243px;
+}
+.burger__items {
+  display: flex !important;
+  padding: 30px 50px 50px 30px !important;
+  flex-direction: column;
+}
+.burger__logo {
+  align-items: center;
+  margin-bottom: 50px;
+  margin-right: 30px;
+}
+.bm-item-list {
+  margin-left: 0% !important;
+  font-family: Gilroy;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 21px;
+  line-height: 140%;
+  /* or 29px */
+
+  color: #ffffff;
+}
+.burger__link:not(:last-child) {
+  margin-bottom: 20px;
+}
+.bm-menu {
+  padding-top: 0;
+}
+.bm-burger-bars.line-style:nth-child(2) {
+  width: 20.83px;
+}
+.cross-style {
+  top: 53.5px;
+}
+.bm-cross-button {
+  height: 25px;
+  width: 25px;
+}
+.bm-cross-button.cross-style {
+  right: 26px !important;
+}
+.bm-cross {
+  width: 27px !important;
+  height: 4.17px !important;
+  border-radius: 20px;
+  background: #ffffff;
+}
+</style>
--- nik/src/components/My-projects/Ojjo/Header.vue
+++ nik/src/components/My-projects/Ojjo/Header.vue
@@ -2,7 +2,8 @@
   <header class="header">
     <div class="header__body">
       <v-container class="container">
-        <ul class="header__items">
+        <Burger :burger="burger" />
+        <ul class="header__items" v-if="burger == false">
           <div class="header__row">
             <div class="start d-flex">
               <li class="header__link" v-for="i in header__link" :key="i">
@@ -30,13 +31,18 @@
         </ul>
       </v-container>
       <div class="header__content">
+        <div class="header-logo" v-if="this.burger">
+          <img src="~@/assets/OJJO/images/logo.png" alt="" srcset="" />
+        </div>
         <div class="header__title">Долго, дорого, богато!</div>
-        <div class="header__catalog">каталог изделий</div>
+        <div class="catalog__button">
+          <div class="header__catalog">каталог изделий</div>
+        </div>
       </div>
-      <div class="header__bottom">
+      <div class="header__bottom" v-if="!brands">
         <v-container>
           <div class="header__row">
-            <div class="header__brands" v-for="i in (1,6)" :key="i">
+            <div class="header__brands" v-for="i in (1, 6)" :key="i">
               <img
                 src="~@/assets/OJJO/images/brands.svg"
                 class="header__img"
@@ -52,13 +58,46 @@
 </template>
 
 <script>
+import Burger from "./Burger.vue";
 export default {
   name: "Ojjo__header",
+  mounted() {
+    this.maxWidth();
+    this.brandsWidth();
+  },
   data() {
     return {
+      burger: null,
       header__link_next: ["Поиск", "Вход/Регистрация"],
       header__link: ["Контрагентам", "Дизайнерам", "Вакансии"],
+      brands: null,
     };
+  },
+  components: {
+    Burger, // Register your component
+  },
+  methods: {
+    maxWidth() {
+      // адаптив для телефона если разрешение будет меньще 600 px то пагинация будет стрелочная
+      setInterval(() => {
+        let w = window.innerWidth;
+        if (w <= 1200) {
+          this.burger = true;
+        } else {
+          this.burger = false;
+        }
+      }, 100);
+    },
+    brandsWidth() {
+      setInterval(() => {
+        let w = window.innerWidth;
+        if (w <= 1400) {
+          this.brands = true;
+        } else {
+          this.brands = false;
+        }
+      }, 100);
+    },
   },
 };
 </script>
